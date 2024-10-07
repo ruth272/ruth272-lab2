acf3605d2ae3506ce3d1c3ac00040aac7926d4dd
ruth272 <143119920+ruth272@users.noreply.github.com>
2024-10-06 23:50:12 -0400

    Update TypeB.java

--- TypeB.java
+++ TypeB.java
@@ -1,1 +1,15 @@
+import java.util.Random;
 
+public class TypeB extends Thing {
+   public TypeB(int var1, int var2, char var3) {
+      super(var1, var2, var3);
+   }
+
+   public void maybeTurn(Random var1) {
+      int var2 = var1.nextInt(3);
+      if (var2 == 1 && var2 == 2) {
+         this.leftTurn();
+      }
+
+   }
+}
