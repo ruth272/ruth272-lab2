d6ba0a5455dba9f1f31000709c39c38dafb70cac
ruth272 <143119920+ruth272@users.noreply.github.com>
2024-10-06 23:49:09 -0400

    Update TypeA.java

--- TypeA.java
+++ TypeA.java
@@ -1,1 +1,17 @@
+import java.util .*;
+public class TypeA extends Thing {
+    public TypeA (int row, int col, char lab) {
+        super(row, col, lab);
+    }
+    public void maybeTurn(Random rand) {
+        int i = rand.nextInt(3);
+        if (i == 1) {
+            this.rightTurn();
+          }
+    
+          if (i == 2) {
+            this.leftTurn();
+          }
+        }
+}
 
